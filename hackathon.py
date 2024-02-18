5b7544b9384486c1a62b70b935e7883210d2d872
ionaxia2013 <108501894+ionaxia2013@users.noreply.github.com>
2024-02-17 20:47:31 -0800

    Update hackathon.py

--- hackathon.py
+++ hackathon.py
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-from transformers import BertTokenizer, BertForSequenceClassification
-import torch_optimizer as optim
-from transformers import torch.optim.AdamW
-import torch
-
-=======
 import numpy as np
 import pandas as pd
 import torch
@@ -13,20 +6,330 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 import transformers
 from transformers import AutoModel, BertTokenizerFast
->>>>>>> 76e254da6f19c0b4b1f92d246d2e2304ccca03d7
+from transformers import AdamW
+from sklearn.utils.class_weight import compute_class_weight
+import copy
 
-device = torch.device("cuda")
-df = pd.read_csv("/Users/ionaxia/Downloads/scam_data.csv")
+df = pd.read_csv("/Users/ionaxia/Downloads/spamdataset.csv")
+device = torch.device("mps")
 
-df['label'].value_counts(normalize = True)
+df['Label'].value_counts(normalize = True)
 
-train_text, temp_text, train_labels, temp_labels = train_test_split(df['text'], df['label'], 
+bert = AutoModel.from_pretrained('bert-base-uncased')
+
+# Load the BERT tokenizer
+tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
+
+
+train_text, temp_text, train_labels, temp_labels = train_test_split(df['Text'], df['Label'], 
                                                                     random_state=2018, 
                                                                     test_size=0.3, 
-                                                                    stratify=df['label'])
+                                                                    stratify=df['Label'])
 
 
 val_text, test_text, val_labels, test_labels = train_test_split(temp_text, temp_labels, 
                                                                 random_state=2018, 
                                                                 test_size=0.5, 
                                                                 stratify=temp_labels)
+
+# tokenize and encode sequences
+tokens_train = tokenizer.batch_encode_plus(
+    train_text.tolist(),
+    max_length = 25,
+    pad_to_max_length=True,
+    truncation=True
+)
+
+tokens_val = tokenizer.batch_encode_plus(
+    val_text.tolist(),
+    max_length = 25,
+    pad_to_max_length=True,
+    truncation=True
+)
+
+tokens_test = tokenizer.batch_encode_plus(
+    test_text.tolist(),
+    max_length = 25,
+    pad_to_max_length=True,
+    truncation=True
+)
+
+train_seq = torch.tensor(tokens_train['input_ids'])
+train_mask = torch.tensor(tokens_train['attention_mask'])
+train_y = torch.tensor(train_labels.tolist())
+
+val_seq = torch.tensor(tokens_val['input_ids'])
+val_mask = torch.tensor(tokens_val['attention_mask'])
+val_y = torch.tensor(val_labels.tolist())
+
+test_seq = torch.tensor(tokens_test['input_ids'])
+test_mask = torch.tensor(tokens_test['attention_mask'])
+test_y = torch.tensor(test_labels.tolist())
+
+print(test_seq, test_mask, len(test_seq[1]), len(test_seq[0]), len(test_mask[1]), len(test_mask[0]))
+
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+
+batch_size = 32
+
+
+train_data = TensorDataset(train_seq, train_mask, train_y)
+
+train_sampler = RandomSampler(train_data)
+
+train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
+
+val_data = TensorDataset(val_seq, val_mask, val_y)
+
+val_sampler = SequentialSampler(val_data)
+
+val_dataloader = DataLoader(val_data, sampler = val_sampler, batch_size=batch_size)
+
+class BERT_Arch(nn.Module):
+
+    def __init__(self, bert):
+        super(BERT_Arch, self).__init__()
+        
+        self.bert = bert 
+        
+        self.dropout = nn.Dropout(0.1)
+      
+        self.relu =  nn.ReLU()
+
+        self.fc1 = nn.Linear(768,512)
+      
+        self.fc2 = nn.Linear(512,2)
+
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    
+
+    def forward(self, sent_id, mask):
+        _, cls_hs = self.bert(sent_id, attention_mask=mask, return_dict=False)
+      
+        x = self.fc1(cls_hs)
+
+        x = self.relu(x)
+
+        x = self.dropout(x)
+
+        x = self.fc2(x)
+      
+        x = self.softmax(x)
+
+        return x
+
+model = BERT_Arch(bert)
+model = model.to(device)
+
+optimizer = AdamW(model.parameters(),lr = 1e-5)
+
+#print(model.keys())
+model.load_state_dict(torch.load("/Users/ionaxia/Documents/best_model.pth"))
+
+
+
+
+class_weights = compute_class_weight(class_weight = "balanced", classes = np.unique(train_labels), y = train_labels)
+
+weights= torch.tensor(class_weights,dtype=torch.float)
+
+weights = weights.to(device)
+
+cross_entropy  = nn.NLLLoss(weight=weights)
+
+model.eval()
+
+dummy_input = torch.tensor([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21,22, 23,24 ,25]])
+dummy_mask = torch.tensor([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21,22, 23,24 ,25]])
+inputs = (dummy_input.to(device), dummy_mask.to(device))
+
+
+# Export the model to ONNX format
+onnx_path = "best_model.onnx"
+
+torch.onnx.export(model, inputs, onnx_path, verbose=True)
+
+#used to train/test model
+'''
+def evaluate():
+    
+    print("\nEvaluating...")
+  
+    # deactivate dropout layers
+    model.eval()
+
+    total_loss, total_accuracy = 0, 0
+    
+    # empty list to save the model predictions
+    total_preds = []
+
+    # iterate over batches
+    for step,batch in enumerate(val_dataloader):
+        
+        # Progress update every 50 batches.
+        if step % 50 == 0 and not step == 0:
+            
+            # Calculate elapsed time in minutes.
+            elapsed = format_time(time.time() - t0)
+            
+            # Report progress.
+            print('  Batch {:>5,}  of  {:>5,}.'.format(step, len(val_dataloader)))
+
+        # push the batch to gpu
+        batch = [t.to(device) for t in batch]
+
+        sent_id, mask, labels = batch
+
+        # deactivate autograd
+        with torch.no_grad():
+            
+            # model predictions
+            preds = model(sent_id, mask)
+
+            # compute the validation loss between actual and predicted values
+            loss = cross_entropy(preds,labels)
+
+            total_loss = total_loss + loss.item()
+
+            preds = preds.detach().cpu().numpy()
+
+            total_preds.append(preds)
+
+    # compute the validation loss of the epoch
+    avg_loss = total_loss / len(val_dataloader) 
+
+    # reshape the predictions in form of (number of samples, no. of classes)
+    total_preds  = np.concatenate(total_preds, axis=0)
+
+    return avg_loss, total_preds
+
+best_valid_loss = float('inf')
+
+#defining epochs
+epochs = 5
+
+# empty lists to store training and validation loss of each epoch
+train_losses=[]
+valid_losses=[]
+
+def train():
+    
+    model.train()
+    total_loss, total_accuracy = 0, 0
+  
+    # empty list to save model predictions
+    total_preds=[]
+  
+    # iterate over batches
+    for step,batch in enumerate(train_dataloader):
+        
+        # progress update after every 50 batches.
+        if step % 10 == 0 and not step == 0:
+            print('  Batch {:>5,}  of  {:>5,}.'.format(step, len(train_dataloader)))
+        
+        # push the batch to gpu
+        batch = [r.to(device) for r in batch]
+ 
+        sent_id, mask, labels = batch
+        
+        # clear previously calculated gradients 
+        model.zero_grad()        
+
+        # get model predictions for the current batch
+        preds = model(sent_id, mask)
+
+        # compute the loss between actual and predicted values
+        loss = cross_entropy(preds, labels)
+
+        # add on to the total loss
+        total_loss = total_loss + loss.item()
+
+        # backward pass to calculate the gradients
+        loss.backward()
+
+        # clip the the gradients to 1.0. It helps in preventing the exploding gradient problem
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+        # update parameters
+        optimizer.step()
+
+        # model predictions are stored on GPU. So, push it to CPU
+        preds=preds.detach().cpu().numpy()
+
+    # append the model predictions
+    total_preds.append(preds)
+
+    # compute the training loss of the epoch
+    avg_loss = total_loss / len(train_dataloader)
+  
+      # predictions are in the form of (no. of batches, size of batch, no. of classes).
+      # reshape the predictions in form of (number of samples, no. of classes)
+    total_preds  = np.concatenate(total_preds, axis=0)
+
+    #returns the loss and predictions
+    return avg_loss, total_preds
+
+#for each epoch
+
+best_model = model
+best_model_state = model.state_dict()
+
+for epoch in range(epochs):
+     
+    print('\n Epoch {:} / {:}'.format(epoch + 1, epochs))
+    
+    #train model
+    train_loss, _ = train()
+    
+    #evaluate model
+    valid_loss, _ = evaluate()
+    
+    #save the best model
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+        best_model_state = copy.deepcopy(model.state_dict())
+        best_model = model
+    
+    # append training and validation loss
+    train_losses.append(train_loss)
+    valid_losses.append(valid_loss)
+    
+    print(f'\nTraining Loss: {train_loss:.3f}')
+    print(f'Validation Loss: {valid_loss:.3f}')
+
+#used to save model
+torch.save(best_model_state, "best_model.pth")
+
+
+with torch.no_grad():
+    preds = model(test_seq.to(device), test_mask.to(device))
+    preds = preds.detach().cpu().numpy()
+
+
+
+preds = np.argmax(preds, axis = 1)
+text = test_text.tolist()
+labels = test_labels.tolist()
+for i in range(len(test_text)):
+    if preds[i] != labels[i]:
+        print(preds[i], labels[i], text[i])
+#print(classification_report(test_y, preds))
+
+
+test_texts =["Hi grandma! I hope you are doing well! I wanted you to make me a blanket."]
+tokens_test2 = tokenizer.batch_encode_plus(
+    test_texts,
+    max_length = 25,
+    pad_to_max_length=True,
+    truncation=True
+)
+test_seq2 = torch.tensor(tokens_test2['input_ids'])
+test_mask2 = torch.tensor(tokens_test2['attention_mask'])
+
+preds = model(test_seq2.to(device), test_mask2.to(device))
+preds = preds.detach().cpu().numpy()
+preds = np.argmax(preds, axis = 1)
+
+print("Predicted Labels:", preds)
+'''
